e2e047a44fbb557fafc0d25054819c46752051d6
yuvrajsingh1821 <imyuvi1218@gmail.com>
2023-07-29 02:35:08 +0530

    update github url

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,7 +9,7 @@ const Header = () => {
        <img src={Logo} alt="" className='bg-white w-12 md:w-14 rounded-[50%]'/>
        
        {/*github button */}
-       <button className='bg-gradient-to-r from-white via-slate-200 to-gray-500 px-3 text-lg md:text-xl font-medium rounded-2xl h-10' onClick={() => window.open('https://github.com/yuvrajsingh1821?tab=repositories')}>Github</button>
+       <button className='bg-gradient-to-r from-white via-slate-200 to-gray-500 px-3 text-lg md:text-xl font-medium rounded-2xl h-10' onClick={() => window.open('https://github.com/yuvrajsingh1821/Article-Summarizer')}>Github</button>
     </div>
   )
 }
